8c6402f864a57f7ef4e63722f09491b80762630b
KloderGit <kloder3@outlook.com>
2017-01-16 00:35:38 +0300

    Модель и данные Преподов

---
+++ Models/Teacher.Model.ts
@@ -0,0 +1,14 @@
+
+export class TeacherModel{
+
+    id: string;
+    guid: string;
+    name: string;
+
+    constructor( id: string, name: string ){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+
