cb6719776b3d88828b6d4c546e47c033c187e066
Kloder <kloder3@gmail.com>
2017-01-24 14:05:02 +0300

    Оформление компонента добавления экзамена Добавлен визард для создания

--- Application/Components/add-examens.component/add-examens.component.ts
+++ Application/Components/add-examens.component/add-examens.component.ts
@@ -1,11 +1,14 @@
 import { DataManager } from './../../Common/DataManager/data-manager';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+declare var $:any;
 
 @Component({
 	moduleId: module.id,
 	selector: 'add-examens',
-	templateUrl: 'add-examens.component.html'
+	templateUrl: 'add-examens.component.html',
+	styleUrls: [ 'add-examens.component.css' ]
 })
 
 export class AddExamensComponent implements OnInit {
@@ -13,13 +16,36 @@ export class AddExamensComponent implements OnInit {
 	date: Date = new Date();
 
 	constructor( private route: ActivatedRoute,
-				 private router: Router,
 				 private dataManager: DataManager){
 		console.log("Создан компонент создания экзаменов");
 	}
 
 	ngOnInit() {
-		let urlParam = this.route.snapshot.params['data'];		
+		let urlParam = this.route.snapshot.params['data'];
 		this.date.setTime(urlParam);
+
+		this.init_jquery();
 	}
+
+	init_jquery(){
+			$('.nav-tabs > li a[title]').tooltip();
+
+			$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+				var $target = $(e.target);
+				if ($target.parent().hasClass('disabled')) {
+					return false;
+				}
+			});
+
+			$(".next-step").click(function (e) {
+				var $active = $('.wizard .nav-tabs li.active');
+				$active.next().removeClass('disabled');
+				$($active).next().find('a[data-toggle="tab"]').click();
+			});
+			$(".prev-step").click(function (e) {
+				var $active = $('.wizard .nav-tabs li.active');
+				$($active).prev().find('a[data-toggle="tab"]').click();
+			});
+	};
+	
 }
